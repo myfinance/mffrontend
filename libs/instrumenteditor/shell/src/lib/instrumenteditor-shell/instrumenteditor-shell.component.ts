983c4390933894c90641236ccdcfa07c9adab6f3
holger <holger@hfischer.org>
2023-05-19 12:25:29 +0200

    test

--- libs/instrumenteditor/shell/src/lib/instrumenteditor-shell/instrumenteditor-shell.component.ts
+++ libs/instrumenteditor/shell/src/lib/instrumenteditor-shell/instrumenteditor-shell.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TenanteditorFeatureTenantcontrollerComponent } from '@mffrontend/tenanteditor/feature-tenantcontroller'
 
 @Component({
   selector: 'mffrontend-instrumenteditor-shell',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TenanteditorFeatureTenantcontrollerComponent],
   templateUrl: './instrumenteditor-shell.component.html',
   styleUrls: ['./instrumenteditor-shell.component.css'],
 })
-export class InstrumenteditorShellComponent {}
+export class InstrumenteditorShellComponent {
+
+}
