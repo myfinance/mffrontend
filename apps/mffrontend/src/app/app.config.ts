fb3d6c2d51d3533f4077ba899216e2b859276aab
holger <holger@hfischer.org>
2023-05-26 08:47:42 +0200

    test

--- apps/mffrontend/src/app/app.config.ts
+++ apps/mffrontend/src/app/app.config.ts
@@ -5,9 +5,10 @@ import {
 } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), provideAnimations()],
+  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), provideAnimations(), {provide: LocationStrategy, useClass: HashLocationStrategy}],
 };
 
 
