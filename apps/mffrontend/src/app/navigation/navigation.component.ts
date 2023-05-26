dc55f69b08c0146ba21dba7a454a79fe4411ba8f
holger <holger@hfischer.org>
2023-05-26 11:45:51 +0200

    menu style

--- apps/mffrontend/src/app/navigation/navigation.component.ts
+++ apps/mffrontend/src/app/navigation/navigation.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'mffrontend-navigation',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatMenuModule, MatToolbarModule],
+  imports: [CommonModule, MatIconModule, MatMenuModule, MatToolbarModule, MatButtonModule],
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
@@ -16,7 +17,7 @@ export class NavigationComponent {
   constructor(private router: Router) { }
 
   home() {
-    this.router.navigate(['/home']);
+    this.router.navigate(['/']);
   }
 
   tenanteditor() {
