7b56cc89fde456cd61daba82b0e2ca4bb29b96c4
holger <holger@hfischer.org>
2023-05-27 09:21:24 +0200

    layout

---
+++ libs/tenanteditor/src/lib/tenantinputform/tenantinputform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mffrontend-tenantinputform',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tenantinputform.component.html',
+  styleUrls: ['./tenantinputform.component.scss'],
+})
+export class TenantinputformComponent {}
---
+++ libs/tenanteditor/src/lib/tenantupdateform/tenantupdateform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mffrontend-tenantupdateform',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tenantupdateform.component.html',
+  styleUrls: ['./tenantupdateform.component.scss'],
+})
+export class TenantupdateformComponent {}
