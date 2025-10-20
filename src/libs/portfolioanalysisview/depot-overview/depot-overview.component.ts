32549a488586ebca76cf796e14e6cf47d5e88843
holgerfischer <holger@hfischer.org>
2025-10-20 20:09:49 +0200

    modify pie chart to view total and percentage value

--- src/libs/portfolioanalysisview/depot-overview/depot-overview.component.ts
+++ src/libs/portfolioanalysisview/depot-overview/depot-overview.component.ts
@@ -16,6 +16,7 @@ export class DepotOverviewComponent {
   positions: Position[] = [];
   data: any;
   options: any;
+  plugins: any[] = [];
 
   constructor(private service: PortfolioAnalysisViewService) {
     this.service.portfolioEventSubject.subscribe({
@@ -40,6 +41,8 @@ export class DepotOverviewComponent {
       const value = aggregation.get(p.securityType) || 0;
       aggregation.set(p.securityType, value + p.value);
     });
+
+    const total = Array.from(aggregation.values()).reduce((a, b) => a + b, 0);
 
     const backgroundColors = [
       documentStyle.getPropertyValue('--blue-500'),
@@ -76,8 +79,55 @@ export class DepotOverviewComponent {
             usePointStyle: true,
             color: textColor
           }
+        },
+        tooltip: {
+          callbacks: {
+            label: function(context: any) {
+              let label = context.label || '';
+              if (label) {
+                label += ': ';
+              }
+              if (context.parsed !== null) {
+                const percentage = (context.parsed / total * 100).toFixed(2);
+                label += new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(context.parsed) + ` (${percentage}%)`;
+              }
+              return label;
+            }
+          }
         }
       }
     };
+
+    this.plugins = [{
+      beforeDraw: (chart: any) => {
+        const ctx = chart.ctx;
+        const txt = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(total);
+        //Get options from the center object in options
+        const sidePadding = 60;
+        const sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2)
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        const centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+        const centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+
+        //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+        const stringWidth = ctx.measureText(txt).width;
+        const elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+
+        // Find out how much the font can be scaled down
+        const widthRatio = elementWidth / stringWidth;
+        const newFontSize = Math.floor(30 * widthRatio);
+        const elementHeight = (chart.innerRadius * 2);
+
+        // Pick a new font size so it will not be larger than the height of label.
+        const fontSizeToUse = Math.min(newFontSize, elementHeight);
+
+        ctx.font = fontSizeToUse + 'px Arial';
+        ctx.fillStyle = 'black';
+
+        //Draw text in center
+        ctx.fillText(txt, centerX, centerY);
+      }
+    }];
   }
 }
