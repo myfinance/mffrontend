3243ac7c6135fb4e083d5ca6796d5acaa72986bb
holgerfischer <holger@hfischer.org>
2025-10-20 19:53:37 +0200

    add Pie Chart for portfolio

--- src/libs/portfolioanalysisview/depot-overview/depot-overview.component.ts
+++ src/libs/portfolioanalysisview/depot-overview/depot-overview.component.ts
@@ -3,28 +3,81 @@ import { Component } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { PortfolioAnalysisViewService } from '../portfolio-analysis-view.service';
 import { Position } from '../../shared/data-access-mfdata/model/position';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-depot-overview',
   standalone: true,
-  imports: [CommonModule, TableModule],
+  imports: [CommonModule, TableModule, ChartModule],
   templateUrl: './depot-overview.component.html',
   styleUrl: './depot-overview.component.scss'
 })
 export class DepotOverviewComponent {
   positions: Position[] = [];
-
+  data: any;
+  options: any;
 
   constructor(private service: PortfolioAnalysisViewService) {
     this.service.portfolioEventSubject.subscribe({
       next:
-        () => this.positions=this.service.getPositions().filter(p=> p.amount!==0),
+        () => this.loadPositions(),
       error:
         (e) => {
           console.error(e);
           alert('Invalid Credentials');
         }
     })
-    this.positions=this.service.getPositions()
+    this.loadPositions();
+  }
+
+  loadPositions() {
+    this.positions = this.service.getPositions().filter(p => p.amount !== 0);
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+
+    const aggregation = new Map<string, number>();
+    this.positions.forEach(p => {
+      const value = aggregation.get(p.securityType) || 0;
+      aggregation.set(p.securityType, value + p.value);
+    });
+
+    const backgroundColors = [
+      documentStyle.getPropertyValue('--blue-500'),
+      documentStyle.getPropertyValue('--yellow-500'),
+      documentStyle.getPropertyValue('--green-500'),
+      documentStyle.getPropertyValue('--red-500'),
+      documentStyle.getPropertyValue('--purple-500'),
+      documentStyle.getPropertyValue('--teal-500')
+    ];
+
+    const hoverBackgroundColors = [
+      documentStyle.getPropertyValue('--blue-400'),
+      documentStyle.getPropertyValue('--yellow-400'),
+      documentStyle.getPropertyValue('--green-400'),
+      documentStyle.getPropertyValue('--red-400'),
+      documentStyle.getPropertyValue('--purple-400'),
+      documentStyle.getPropertyValue('--teal-400')
+    ];
+
+    this.data = {
+      labels: Array.from(aggregation.keys()),
+      datasets: [
+        {
+          data: Array.from(aggregation.values()),
+          backgroundColor: backgroundColors.slice(0, aggregation.size),
+          hoverBackgroundColor: hoverBackgroundColors.slice(0, aggregation.size)
+        }
+      ]
+    };
+    this.options = {
+      plugins: {
+        legend: {
+          labels: {
+            usePointStyle: true,
+            color: textColor
+          }
+        }
+      }
+    };
   }
 }
